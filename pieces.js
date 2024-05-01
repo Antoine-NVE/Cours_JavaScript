69a1416cae1d27c26bf29e38df57302d0be9edfe
Antoine-NVE <antoinenavette@gmail.com>
2024-05-01 10:06:32 +0200

    Mettez à jour l’affichage de la page web

--- pieces.js
+++ pieces.js
@@ -1,42 +1,45 @@
-const reponse = await fetch('pieces-autos.json');
-const pieces = await reponse.json();
+const pieces = await fetch('pieces-autos.json').then((pieces) => pieces.json());
 const sectionFiches = document.querySelector('.fiches');
 
-for (let i = 0; i < pieces.length; i++) {
-    const article = pieces[i];
+function genererPieces(pieces) {
+    for (let i = 0; i < pieces.length; i++) {
+        const article = pieces[i];
 
-    const imageElement = document.createElement('img');
-    imageElement.src = article.image;
+        const imageElement = document.createElement('img');
+        imageElement.src = article.image;
 
-    const nomElement = document.createElement('h2');
-    nomElement.innerText = article.nom;
+        const nomElement = document.createElement('h2');
+        nomElement.innerText = article.nom;
 
-    const prixElement = document.createElement('p');
-    prixElement.innerText = `Prix : ${article.prix} € (${
-        article.prix < 35 ? '€' : '€€€'
-    })`;
+        const prixElement = document.createElement('p');
+        prixElement.innerText = `Prix : ${article.prix} € (${
+            article.prix < 35 ? '€' : '€€€'
+        })`;
 
-    const categorieElement = document.createElement('p');
-    categorieElement.innerText = article.categorie ?? '(aucune catégorie)';
+        const categorieElement = document.createElement('p');
+        categorieElement.innerText = article.categorie ?? '(aucune catégorie)';
 
-    const descriptionElement = document.createElement('p');
-    descriptionElement.innerText =
-        article.description ?? 'Pas de description pour le moment.';
+        const descriptionElement = document.createElement('p');
+        descriptionElement.innerText =
+            article.description ?? 'Pas de description pour le moment.';
 
-    const stockElement = document.createElement('p');
-    stockElement.innerText = article.disponibilite
-        ? 'En stock'
-        : 'Rupture de stock';
+        const stockElement = document.createElement('p');
+        stockElement.innerText = article.disponibilite
+            ? 'En stock'
+            : 'Rupture de stock';
 
-    const pieceElement = document.createElement('article');
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nomElement);
-    pieceElement.appendChild(prixElement);
-    pieceElement.appendChild(categorieElement);
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(stockElement);
-    sectionFiches.appendChild(pieceElement);
+        const pieceElement = document.createElement('article');
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(stockElement);
+        sectionFiches.appendChild(pieceElement);
+    }
 }
+
+genererPieces(pieces);
 
 const boutonTrier = document.querySelector('.btn-trier');
 boutonTrier.addEventListener('click', () => {
@@ -44,7 +47,8 @@ boutonTrier.addEventListener('click', () => {
     piecesOrdonnees.sort((a, b) => {
         return a.prix - b.prix;
     });
-    console.log(piecesOrdonnees);
+    document.querySelector('.fiches').innerHTML = '';
+    genererPieces(piecesOrdonnees);
 });
 
 const boutonFiltrer = document.querySelector('.btn-filtrer');
@@ -52,7 +56,8 @@ boutonFiltrer.addEventListener('click', () => {
     const piecesFiltrees = pieces.filter((piece) => {
         return piece.prix <= 35;
     });
-    console.log(piecesFiltrees);
+    document.querySelector('.fiches').innerHTML = '';
+    genererPieces(piecesFiltrees);
 });
 
 const boutonDecroissant = document.querySelector('.btn-decroissant');
@@ -61,7 +66,8 @@ boutonDecroissant.addEventListener('click', () => {
     piecesOrdonnees.sort((a, b) => {
         return b.prix - a.prix;
     });
-    console.log(piecesOrdonnees);
+    document.querySelector('.fiches').innerHTML = '';
+    genererPieces(piecesOrdonnees);
 });
 
 const boutonNodesc = document.querySelector('.btn-nodesc');
@@ -69,7 +75,8 @@ boutonNodesc.addEventListener('click', () => {
     const piecesFiltrees = pieces.filter((piece) => {
         return piece.description;
     });
-    console.log(piecesFiltrees);
+    document.querySelector('.fiches').innerHTML = '';
+    genererPieces(piecesFiltrees);
 });
 
 const noms = pieces.map((piece) => piece.nom);
@@ -107,3 +114,12 @@ for (let i = 0; i < nomsDisponibles.length; i++) {
 }
 
 document.querySelector('.disponibles').appendChild(disponiblesElement);
+
+const inputPrixMax = document.querySelector('#prix-max');
+inputPrixMax.addEventListener('input', () => {
+    const piecesFiltrees = pieces.filter((piece) => {
+        return piece.prix <= inputPrixMax.value;
+    });
+    document.querySelector('.fiches').innerHTML = '';
+    genererPieces(piecesFiltrees);
+});
